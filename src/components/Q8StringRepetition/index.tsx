4f062e61e8a6cfac6fd1016624d355c076511879
Paula Nader <paulampaan@gmail.com>
2022-03-17 16:28:37 -0300

    Correção questão 8

--- src/components/Q8StringRepetition/index.tsx
+++ src/components/Q8StringRepetition/index.tsx
@@ -8,8 +8,8 @@ const Q8StringRepetition: React.FC<IQ8StringRepetitionProps> = ({
     number,
 }) => {
     let phrase = '';
-    for (const i = 0; i < number - 1; i + 1) {
-        phrase = value;
+    for (let i = 0; i < number - 1; i += 1) {
+        phrase += value;
     }
     return <span>{phrase}</span>;
 };
