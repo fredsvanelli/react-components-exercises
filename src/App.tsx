a43233de0048653599a0a2245e95d3ebf22503b4
Paula Nader <paulampaan@gmail.com>
2022-03-17 18:15:55 -0300

    Questão 9

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import Q1String from './components/Q1String';
 import Q2Uppercase from './components/Q2Uppercase';
+import Q9CharRepeatingWithSpaceAndComma from './components/Q9CharRepeatingWithSpaceAndComma';
 
 const App: React.FC = () => {
     return (
@@ -83,7 +84,9 @@ const App: React.FC = () => {
                 <br /> Prop 2: 5.
                 <br /> Retorno: Oi, Oi, Oi, Oi, Oi
             </h2>
-            <div className="card p-3 mb-4">{/* Insira a resposta aqui */}</div>
+            <div className="card p-3 mb-4">
+                <Q9CharRepeatingWithSpaceAndComma value="Oi" number={5} />
+            </div>
 
             <h2 className="fs-6">
                 10. Crie um componente que recebe duas strings e retorna a que
---
+++ src/components/Q9CharRepeatingWithSpaceAndComma/index.tsx
@@ -0,0 +1,18 @@
+interface IQ9CharRepeatingWithSpaceAndCommaProps {
+    value: string;
+    number: number;
+}
+
+const Q9CharRepeatingWithSpaceAndComma: React.FC<
+    IQ9CharRepeatingWithSpaceAndCommaProps
+> = ({ value, number }) => {
+    const myArray = [value];
+
+    for (let i = 0; i < number; i += 1) {
+        myArray.push(value);
+    }
+
+    return <span>{myArray.join(', ')}</span>;
+};
+
+export default Q9CharRepeatingWithSpaceAndComma;
