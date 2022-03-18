8a35d7f90ad1f04cd7287b25a434e6e039041112
Paula Nader <paulampaan@gmail.com>
2022-03-18 15:23:49 -0300

    Melhorando a lógica da questão 13

--- src/components/Q13ReplacingEvents/index.tsx
+++ src/components/Q13ReplacingEvents/index.tsx
@@ -8,20 +8,6 @@ const Q13ReplacingEvents: React.FC<IQ13ReplacingEventsProps> = ({
     value,
     event,
     change,
-}) => {
-    let count = 0;
-    for (let i = 0; i < value.length; i += 1) {
-        if (value[i] === event) {
-            count += 1;
-        }
-    }
-
-    let newValue = value;
-    for (let i = 0; i < count; i += 1) {
-        newValue = newValue.replace(event, change);
-    }
-
-    return <span>{newValue}</span>;
-};
+}) => <span>{value.replaceAll(event, change)}</span>;
 
 export default Q13ReplacingEvents;
