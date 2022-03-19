ad83f08abb2e24b605c5ef0a36c6b132da6a8b74
Paula Nader <paulampaan@gmail.com>
2022-03-19 10:08:15 -0300

    Incluindo o 10

--- src/components/Q23Random/index.tsx
+++ src/components/Q23Random/index.tsx
@@ -1,3 +1,5 @@
-const Q23Random: React.FC = () => <span>{Math.floor(Math.random() * 10)}</span>;
+const Q23Random: React.FC = () => (
+    <span>{Math.floor(Math.random() * 10 + 1)}</span>
+);
 
 export default Q23Random;
